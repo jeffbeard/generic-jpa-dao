849d7291730cc3305ed3c2b7c26125f93688f234
U-ZORG\beardj <beardj@zorg.(none)>
2011-04-25 21:15:36 -0600

    initial import

---
+++ src/main/java/org/firebyte/generic/dao/GenericDAO.java
@@ -0,0 +1,114 @@
+/*
+ * $Id$
+ * Created on Mar 25, 2011 by beardj 
+ */
+package org.firebyte.generic.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Generic DAO interface for basic CRUD functionality
+ * 
+ * @author <a href="mailto:jeff@firebyte.org">Jeff Beard</a>
+ *
+ */
+public interface GenericDAO<T, PK extends Serializable> {
+	
+   /**
+     * Get the Class of the entity.
+     *
+     * @return the class
+     */
+    Class<T> getEntityClass();
+
+    /**
+     * Find an entity by its primary key
+     *
+     * @param id the primary key
+     * @return the entity
+     */
+    T get(final PK id);
+
+    /**
+     * Load all entities.
+     *
+     * @return the list of entities
+     */
+    List<T> findAll();
+
+    /**
+     * Find entities based on an example.
+     *
+     * @param exampleInstance the example
+     * @return the list of entities
+     */
+    List<T> findByExample(final T exampleInstance);
+
+    /**
+     * Find using a named query.
+     *
+     * @param queryName the name of the query
+     * @param params the query parameters
+     *
+     * @return the list of entities
+     */
+    List<T> findByNamedQuery(final String queryName, Object... params);
+
+    /**
+     * Find using a named query.
+     *
+     * @param queryName the name of the query
+     * @param params the query parameters
+     *
+     * @return the list of entities
+     */
+    List<T> findByNamedQueryAndNamedParams(final String queryName, final Map<String, ?extends Object> params);
+
+    /**
+     * Count all entities.
+     *
+     * @return the number of entities
+     */
+    int countAll();
+
+    /**
+     * Count entities based on an example.
+     *
+     * @param exampleInstance the search criteria
+     * @return the number of entities
+     */
+    int countByExample(final T exampleInstance);
+ 
+    /**
+     * Save an entity. This can be either a INSERT or UPDATE in the database.
+     * 
+     * @param entity the entity to save
+     * 
+     * @return the saved entity
+     */
+    T save(final T entity);
+
+    /**
+     * delete an entity from the database.
+     * 
+     * @param entity the entity to delete
+     */
+    void remove(final T entity);
+    
+    /**
+     * Delete an entity from the database by PK
+     * 
+     * @param id
+     */
+    void remove(final PK id);
+    
+    /**	
+     * Setting for EntityManager DI
+     * @param entityManager
+     */
+    void setEntityManager(EntityManager entityManager);
+}
---
+++ src/main/java/org/firebyte/generic/dao/jpa/GenericDAOJPA.java
@@ -0,0 +1,248 @@
+/*
+ * $Id$
+ * Created on Mar 27, 2011 by beardj 
+ */
+package org.firebyte.generic.dao.jpa;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.firebyte.generic.dao.GenericDAO;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Projections;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * JPA Hibernate implementation of the GenericDAO
+ * 
+ * @author <a href="mailto:jeff@firebyte.org">Jeff Beard</a>
+ *
+ */
+public class GenericDAOJPA<T, ID extends Serializable> implements GenericDAO<T, ID>  {
+
+	private final Class<T> persistentClass;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@SuppressWarnings("unchecked")
+	public GenericDAOJPA() {
+		this.persistentClass = (Class<T>) ((ParameterizedType) 
+			getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+	}
+
+	public GenericDAOJPA(final Class<T> persistentClass) {
+		super();
+		this.persistentClass = persistentClass;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#countAll()
+	 */
+	@Override
+	public int countAll() {
+		return countByCriteria();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#countByExample(java.lang.Object)
+	 */
+	@Override
+	public int countByExample(final T exampleInstance) {
+		
+		Session session = (Session) getEntityManager().getDelegate();
+		Criteria crit = session.createCriteria(getEntityClass());
+		crit.setProjection(Projections.rowCount());
+		crit.add(Example.create(exampleInstance));
+
+		return (Integer) crit.list().get(0);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#findAll()
+	 */
+	@Override
+	public List<T> findAll() {
+		return findByCriteria();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#findByExample(java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByExample(final T exampleInstance) {
+		Session session = (Session) getEntityManager().getDelegate();
+		Criteria crit = session.createCriteria(getEntityClass());
+		final List<T> result = crit.list();  
+		return result; 
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#findById(java.io.Serializable)
+	 */
+	@Override
+	public T get(final ID id) {
+		final T result = getEntityManager().find(persistentClass, id);
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#findByNamedQuery(java.lang.String, java.lang.Object[])
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByNamedQuery(final String name, Object... params) {
+		
+		Query query = getEntityManager().createNamedQuery(name);
+
+		for (int i = 0; i < params.length; i++) {
+			query.setParameter(i + 1, params[i]);
+		}
+
+		final List<T> result = (List<T>) query.getResultList();
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#findByNamedQueryAndNamedParams(java.lang.String, java.util.Map)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByNamedQueryAndNamedParams(final String name,
+			final Map<String, ? extends Object> params) {
+		
+		Query query = getEntityManager().createNamedQuery(name);
+
+		for (final Map.Entry<String, ? extends Object> param : params.entrySet()) {
+			query.setParameter(param.getKey(), param.getValue());
+		}
+
+		final List<T> result = (List<T>) query.getResultList();
+		
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#save(java.lang.Object)
+	 */
+	@Override
+	@Transactional
+	public T save(T entity) {
+		final T savedEntity = getEntityManager().merge(entity);
+		return savedEntity;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#delete(java.lang.Object)
+	 */
+	@Override
+	public void remove(T entity) {
+		getEntityManager().remove(entity);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.repository.GenericRepository#remove(java.io.Serializable)
+	 */
+	@Override
+	public void remove(ID id) {
+		getEntityManager().remove(this.get(id));
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.cohomefinder.dao.GenericRepository#getEntityClass()
+	 */
+	@Override
+	public Class<T> getEntityClass() {
+		return persistentClass;
+	}
+
+	/**
+	 * Set the JPA entity manager to use.
+	 *
+	 * @param entityManager
+	 */
+	// @Required spring framework
+	@PersistenceContext
+	public void setEntityManager(final EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	/**
+	 * Use this inside subclasses as a convenience method.
+	 */
+	protected List<T> findByCriteria(final Criterion... criterion) {
+		return findByCriteria(-1, -1, criterion);
+	}
+
+	/**
+	 * Use this inside subclasses as a convenience method.
+	 */
+	@SuppressWarnings("unchecked")
+	protected List<T> findByCriteria(final int firstResult, final int maxResults, final Criterion... criterion) {
+		
+		Session session = (Session) getEntityManager().getDelegate();
+		Criteria crit = session.createCriteria(getEntityClass());
+
+		for (final Criterion c : criterion) {
+			crit.add(c);
+		}
+
+		if (firstResult > 0) {
+			crit.setFirstResult(firstResult);
+		}
+
+		if (maxResults > 0) {
+			crit.setMaxResults(maxResults);
+		}
+
+		final List<T> result = crit.list();
+		return result;
+	}
+
+	/**
+	 * Get counts by Criteria
+	 * 
+	 * @param criterion
+	 * @return int
+	 */
+	protected int countByCriteria(Criterion... criterion) {
+		
+		Session session = (Session) getEntityManager().getDelegate();
+		
+		Criteria crit = session.createCriteria(getEntityClass());
+		crit.setProjection(Projections.rowCount());
+
+		for (final Criterion c : criterion) {
+			crit.add(c);
+		}
+
+		return (Integer) crit.list().get(0);
+	}
+}
